9b610b8b2a781b3d92a870f1569478180552ec9a
Chris Gilling <cgilling@iparadigms.com>
2018-12-01 15:29:35 -0800

    add yaml and envvar configuration options
    If --config flag is used, a yaml config file will be read. After
    that, environment variables will be applied. The environment
    is read even if the yaml config is specified and anything in the
    environment will override the config file.

--- server.go
+++ server.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"text/tabwriter"
 
 	"github.com/cgilling/plantuml-proxy/plantuml"
 	"github.com/gorilla/handlers"
 	"github.com/julienschmidt/httprouter"
+	"github.com/kelseyhightower/envconfig"
+	yaml "gopkg.in/yaml.v1"
 )
 
-const DefaultListenAddr = ":8080"
+const (
+	DefaultListenAddr   = ":8080"
+	EnvPrefix           = "PLANTUML_PROXY"
+	ModifiedTableFormat = `This application can be configured via the environment. The following environment
+variables can be used:
+KEY	TYPE	DESCRIPTION
+{{range .}}{{usage_key .}}	{{usage_type .}}	{{usage_description .}}
+{{end}}`
+)
+
+var (
+	configPath         = flag.String("config", "", "path to yaml config file, if not provided, env vars are used")
+	printDefaultConfig = flag.Bool("print-default-config", false, "if set to true, will print the default config yaml, end exit")
+)
 
 type Config struct {
-	ListenAddr string
-	ServerURL  string
+	ListenAddr string `yaml:"listen_addr" json:"listen_addr" envconfig:"listen_addr" desc:"address to listen on for requests"`
+	ServerURL  string `yaml:"plantuml_url" json:"plantuml_url" envconfig:"plantuml_url" desc:"url for plantuml server to handle conversion requests"`
 }
 
 type App struct {
@@ -26,6 +44,39 @@ type App struct {
 func main() {
 	var config Config
 	applyConfigDefault(&config)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage of %s
+plantuml-proxy is a simple proxy service that allows for posting of
+unencoded uml files for conversion rather than needing to use the custom
+plantuml url encoding scheme.
+Flags:
+`, os.Args[0])
+		flag.PrintDefaults()
+		fmt.Println("")
+		tabs := tabwriter.NewWriter(os.Stderr, 1, 0, 4, ' ', 0)
+		envconfig.Usagef(EnvPrefix, &config, tabs, ModifiedTableFormat)
+		tabs.Flush()
+	}
+
+	flag.Parse()
+
+	if *printDefaultConfig {
+		b, _ := yaml.Marshal(&config)
+		fmt.Print(string(b))
+		return
+	}
+	if *configPath != "" {
+		b, err := ioutil.ReadFile(*configPath)
+		if err != nil {
+			log.Fatalf("failed to read config file %q: %v", *configPath, err)
+		}
+		if err = yaml.Unmarshal(b, &config); err != nil {
+			log.Fatalf("failed to parse config yaml: %v", err)
+		}
+	}
+	if err := envconfig.Process(EnvPrefix, &config); err != nil {
+		log.Fatalf("failed to process environment: %v", err)
+	}
 
 	app, err := NewApp(config)
 	if err != nil {
